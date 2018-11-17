3919f739ee420c5cf4e4cc2aed0b90046927ce0a
Kevin Gliewe <kevingliewe@outlook.de>
2018-11-17 02:23:19 +0100

    ➕ Add mod 'CustomRenderDist'

---
+++ CustomRenderDist/CustomRenderDist/CustomRenderDist/Patch.cs
@@ -0,0 +1,25 @@
+﻿using GCore.Logging;
+using Harmony;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CustomRenderDist
+{
+    [HarmonyPatch(typeof(CameraManager))]
+    [HarmonyPatch("SetClipDist01")]
+    internal class Patch_CameraManager_SetClipDist01
+    {
+        static void Prefix(CameraManager __instance, ref float clipDist01)
+        {
+            clipDist01 *= 5;
+            Log.Info("Set clipDist to " + clipDist01);
+        }
+
+        static void Postfix(CameraManager __instance)
+        {
+            Log.Info("CameraManager.DrawDist = " + __instance.DrawDist);
+        }
+    }
+}
---
+++ CustomRenderDist/CustomRenderDist/CustomRenderDist/QMod.cs
@@ -0,0 +1,25 @@
+﻿using GCore.Logging;
+using Harmony;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace CustomRenderDist
+{
+    public class QMod
+    {
+        public static void main()
+        {
+            try {
+                HarmonyInstance.Create("net.killergodfisch.CustomRenderDist").PatchAll(Assembly.GetExecutingAssembly());
+            } catch (Exception ex) {
+                Log.Error("Error while loading CustomRenderDist, see logs fpr further informations!");
+                throw ex;
+            }
+
+            Log.Info("CustomRenderDist Loaded");
+        }
+    }
+}
